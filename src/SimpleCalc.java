b7d213918da9513f37dde99fd22b91d5db46edb4
Amanuel <amanuelfeyissa45@gmail.com>
2020-08-18 17:53:51 +0300

    new simple calculator class

---
+++ src/SimpleCalc.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SimpleCalc{
+
+    public double sum(double num1, double num2){
+        return num1 + num2;
+    }
+    public double subtract(double num1, double num2){
+        return num1 - num2;
+    }
+    public double multiply(double num1, double num2){
+        return num1 * num2;
+    }
+    public double divide(double num1, double num2){
+        return num1 / num2;
+    }
+    public double remainder(double num1, double num2) {
+        return num1 % num2;
+    }
+
+}
