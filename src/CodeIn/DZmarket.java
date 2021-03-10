c5b264413dfb95db01ab2dc4821a74e4133608bd
Amanuel <amanuelfeyissa45@gmail.com>
2021-03-10 20:26:58 +0300

    made an out put display for the DZ object using the calcTotal method

--- src/CodeIn/DZmarket.java
+++ src/CodeIn/DZmarket.java
@@ -4,7 +4,7 @@ public class DZmarket {
 
     public static void main(String[] args){
         Market DZ = new Market("Orange",12);
-        DZ.calcTotal();
+
         System.out.println("The Calculated Total Value is " + DZ.calcTotal());
     }
 }
